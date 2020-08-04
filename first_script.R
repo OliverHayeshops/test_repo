72be98a52c47bc9cc3b08e4fdb73eee64fc780bd
OliverHayeshops <69173942+OliverHayeshops@users.noreply.github.com>
2020-08-05 09:02:11 +1000

    Az

--- first_script.R
+++ first_script.R
@@ -19,3 +19,5 @@ scatter <- ggplot(data=iris, aes(x = Sepal.Length, y = Sepal.Width))
 scatter + geom_point(aes(color=Species, shape=Species)) +
   xlab("Sepal Length") +  ylab("Sepal Width") +
   ggtitle("Sepal Length-Width")
+
+#so what happens now? asd
